6213bfbdbf2ad9793becd40eae9666184f1460f3
ibrahim-yagmur <iboygmr55@gmail.com>
2021-04-24 17:55:07 +0300

    java kamp proje 1

---
+++ src/javaKamp.java
@@ -0,0 +1,78 @@
+public class javaKamp {
+    public static void main(String[] args) {
+        //camelCase
+        //Don' repeat yourself
+        String InternetSubeButonu = "Internet Subeye Gir";
+        double dolarDun = 8.20;
+        double dolarBugun = 8.20;
+        int vade = 36;
+        boolean dustuMu = false;
+        System.out.println(InternetSubeButonu);
+
+        if (dolarBugun < dolarDun) {
+
+            System.out.println("Dolar Düştü resmi");
+
+        } else if (dolarBugun > dolarDun) {
+
+            System.out.println("Dolar Yükseldi  resmi ");
+
+        } else {
+            System.out.println("Dolar eşittir resmi ");
+        }
+        String kredi1 = "Hızlı Kredi";
+        String kredi2 = "Mutlu emekli kredisi";
+        String kredi3 = "Konut kredisi";
+        String kredi4 = "Çiftçi kredisi";
+        String kredi5 = "Msb kredisi";
+        String kredi6 = "Meb kredisi";
+        String kredi7 = "Kültür bakanlığı kredisi";
+
+        System.out.println(kredi1);
+        System.out.println(kredi2);
+        System.out.println(kredi3);
+        System.out.println(kredi4);
+        System.out.println(kredi5);
+        System.out.println(kredi6);
+        System.out.println(kredi7);
+
+        String[] krediler =
+                {"Hızlı Kredi",
+                        "Mutlu emekli kredisi",
+                        "Konut kredisi",
+                        "Çiftçi kredisi",
+                        "Msb kredisi",
+                        "Meb kredisi",
+                        "Kültür bakanlığı kredisi"
+                };
+        //foreach Döngüsü
+        for (String kredi : krediler) {
+            System.out.println(kredi);
+        }
+
+        for (int i = 0; i < krediler.length; i++) {
+            System.out.println(krediler[i]);
+        }
+        System.out.println(krediler[6]);
+        int sayi1 = 10;
+        int sayi2 = 20;
+        sayi1 = sayi2;
+        sayi2 = 100;
+        System.out.println(sayi1);
+        int[] sayilar1 = {1, 2, 3, 4, 5};
+        int[] sayilar2 = {10, 20, 30, 40, 50};
+        sayilar1 = sayilar2;
+        sayilar2[0] = 100;
+        System.out.println(sayilar1[0]);
+
+
+        String sehir1 = "Ankara";
+        String sehir2 = "İstanbul";
+        sehir1 = sehir2;
+        sehir2 = "İzmir";
+        System.out.println(sehir1);
+
+    }
+}
+
+
